ec3900663a186a9557698d3c6e29ad985be7eabd
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2017-12-10 14:24:03 +0000

    Initial commit

---
+++ src/main/java/quimp/plugin/MedianSnakeFilter_.java
@@ -0,0 +1,212 @@
+package quimp.plugin;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JSpinner;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.scijava.vecmath.Point2d;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.celldynamics.quimp.PropertyReader;
+import com.github.celldynamics.quimp.ViewUpdater;
+import com.github.celldynamics.quimp.plugin.IQuimpPluginSynchro;
+import com.github.celldynamics.quimp.plugin.ParamList;
+import com.github.celldynamics.quimp.plugin.QuimpPluginException;
+import com.github.celldynamics.quimp.plugin.snakes.IQuimpBOAPoint2dFilter;
+import com.github.celldynamics.quimp.plugin.utils.IPadArray;
+import com.github.celldynamics.quimp.plugin.utils.QWindowBuilder;
+import com.github.celldynamics.quimp.plugin.utils.QuimpDataConverter;
+
+/**
+ * Interpolation of points (X,Y) by means of running median method.
+ * 
+ * @author p.baniukiewicz
+ * @date 10 Dec 2017
+ *
+ */
+public class MedianSnakeFilter_ extends QWindowBuilder implements IQuimpBOAPoint2dFilter, IPadArray,
+        IQuimpPluginSynchro, ChangeListener, ActionListener {
+
+  static final Logger LOGGER = LoggerFactory.getLogger(MedianSnakeFilter_.class.getName());
+  private QuimpDataConverter xyData; //!< input List converted to separate X and Y arrays
+  private int window; //!< size of processing window
+  private ParamList uiDefinition; //!< Definition of UI
+  protected ViewUpdater qcontext; //!< remember QuimP context to recalculate and update its view 
+
+  /**
+   * Create running mean filter.
+   * 
+   * <p>All default parameters should be declared here. Non-default are passed by
+   * setPluginConfig(ParamList)
+   */
+  public MedianSnakeFilter_() {
+    LOGGER.trace("Entering constructor");
+    this.window = 3; // default value
+    LOGGER.debug("Set default parameter: window=" + window);
+    // create UI using QWindowBuilder
+    uiDefinition = new ParamList(); // will hold ui definitions
+    // configure window, names of UI elements are also names of variables
+    // exported/imported by set/getPluginConfig
+    uiDefinition.put("name", "MeanFilter"); // name of win
+    uiDefinition.put("window", "spinner: 1: 21: 2:" + Integer.toString(window));
+    uiDefinition.put("help", "Window must be uneven. Set 1 to switch filter off.");
+    buildWindow(uiDefinition); // construct ui (not shown yet)
+  }
+
+  /**
+   * Attach data to process.
+   * 
+   * <p>Data are as list of vectors defining points of polygon. Passed points should be sorted
+   * according to a clockwise or anti-clockwise direction
+   * 
+   * @param data Polygon points
+   * @see com.github.celldynamics.quimp.plugin.snakes.IQuimpBOAPoint2dFilter#attachData(List)
+   */
+  @Override
+  public void attachData(List<Point2d> data) {
+    LOGGER.trace("Entering attachData");
+    if (data == null) {
+      return;
+    }
+    xyData = new QuimpDataConverter(data); // helper for converting from List<Vector2d> to X[], Y[]
+  }
+
+  /**
+   * Perform interpolation of data by a moving median filter with given window
+   * 
+   * <p>By default uses CIRCULAR padding. The window must be uneven, positive and shorter than
+   * data vector. X and Y coordinates of points are smoothed separately.
+   * 
+   * @return Filtered points as list of Vector2d objects
+   * @throws QuimpPluginException when: - window is even - window is longer or equal processed
+   *         data - window is negative
+   */
+  @Override
+  public List<Point2d> runPlugin() throws QuimpPluginException {
+    // collect actual parameters from UI
+    window = getIntegerFromUI("window");
+    LOGGER.debug(String.format("Run plugin with params: window %d", window));
+
+    // do filtering
+    int cp = window / 2; // left and right range of window
+    List<Point2d> out = new ArrayList<Point2d>();
+
+    return out;
+  }
+
+  /**
+   * This method should return a flag word that specifies the filters capabilities.
+   * 
+   * @return Configuration codes
+   * @see com.github.celldynamics.quimp.plugin.IQuimpCorePlugin
+   * @see com.github.celldynamics.quimp.plugin.IQuimpPlugin#setup()
+   */
+  @Override
+  public int setup() {
+    LOGGER.trace("Entering setup");
+    return DOES_SNAKES;
+  }
+
+  /**
+   * Configure plugin and overrides default values.
+   * 
+   * <p>It is called by plugin user to pass configuration to plugin.
+   * 
+   * <p>Supported keys: <i>window</i> - size of window
+   * 
+   * @param par configuration as pairs [key,val]. Keys are defined by plugin creator and plugin
+   *        user do not modify them.
+   * @throws QuimpPluginException on wrong parameters list or wrong parameter conversion
+   * @see com.github.celldynamics.quimp.plugin.IQuimpPlugin#setPluginConfig(ParamList)
+   */
+  @Override
+  public void setPluginConfig(final ParamList par) throws QuimpPluginException {
+    try {
+      LOGGER.debug("Set params: " + par);
+      setValues(par); // populate loaded values to UI
+      window = par.getIntValue("window");
+    } catch (Exception e) {
+      e.printStackTrace();
+      // we should never hit this exception as parameters are not touched
+      // by caller they are only passed to configuration saver and
+      // restored from it
+      throw new QuimpPluginException("Wrong input argument-> " + e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Transfer plugin configuration to QuimP.
+   * 
+   * <p>Only parameters mapped to UI by QWindowBuilder are supported directly by getValues() Any
+   * other parameters created outside QWindowBuilder should be added here manually.
+   */
+  @Override
+  public ParamList getPluginConfig() {
+    return getValues();
+  }
+
+  @Override
+  public int showUi(boolean val) {
+    LOGGER.debug("Got message to show UI");
+    toggleWindow(val);
+    return 0;
+  }
+
+  @Override
+  public String getVersion() {
+    String trimmedClassName = getClass().getSimpleName();
+    trimmedClassName = trimmedClassName.substring(0, trimmedClassName.length() - 1); // no _
+    // _ at the end of class does not appears in final jar name, we need it to
+    // distinguish between plugins
+    return PropertyReader.readProperty(getClass(), trimmedClassName,
+            "quimp/plugin/plugin.properties", "internalVersion");
+  }
+
+  @Override
+  public void attachContext(ViewUpdater b) {
+    qcontext = b;
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    Object b = e.getSource();
+    if (b == applyB) { // pressed apply, copy ui data to plugin
+      qcontext.updateView();
+    }
+  }
+
+  @Override
+  public void stateChanged(ChangeEvent ce) {
+    Object source = ce.getSource();
+    JSpinner s = (JSpinner) ui.get("window"); // get ui element
+    if (source == s) { // check if this event concerns it
+      LOGGER.debug("Spinner used");
+      if (((Double) s.getValue()).intValue() % 2 == 0) {
+        s.setValue((Double) s.getValue() + 1);
+      }
+    }
+    if (isWindowVisible() == true) {
+      qcontext.updateView();
+    }
+  }
+
+  @Override
+  public void buildWindow(final ParamList def) {
+    super.buildWindow(def); // window must be built first
+    // attach listener to selected ui
+    ((JSpinner) ui.get("window")).addChangeListener(this);
+    applyB.addActionListener(this); // attach listener to apply button
+  }
+
+  @Override
+  public String about() {
+    return "Running median filter over outline\nAuthor: Piotr Baniukiewicz\n"
+            + "mail: p.baniukiewicz@warwick.ac.uk";
+  }
+}
---
+++ src/test/java/quimp/plugin/MedianFilterTest.java
@@ -0,0 +1,90 @@
+package quimp.plugin;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.scijava.vecmath.Point2d;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.celldynamics.quimp.plugin.ParamList;
+import com.github.celldynamics.quimp.plugin.QuimpPluginException;
+
+/**
+ * Test runner for Interpolate class.
+ * 
+ * <p>Contain only simple non-parameterized tests
+ * 
+ * @author p.baniukiewicz
+ *
+ */
+public class MedianFilterTest {
+
+  private List<Point2d> testcase;
+  static final Logger LOGGER = LoggerFactory.getLogger(MedianFilterTest.class.getName());
+
+  /**
+   * Called after construction but before tests.
+   * 
+   * @throws Exception Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    testcase = new ArrayList<Point2d>();
+    for (int i = 1; i <= 10; i++) {
+      testcase.add(new Point2d(i, i));
+    }
+  }
+
+  /**
+   * tearDown.
+   * 
+   * @throws java.lang.Exception Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+    testcase = null;
+  }
+
+  /**
+   * Test of getInterpolationMean method.
+   * 
+   * <p>Pre: Vector of 1-10 elements
+   * 
+   * <p>Post: Running mean for window 3: [4.3333 2.0000 3.0000 4.0000 5.0000 6.0000 7.0000 8.0000
+   * 9.0000 6.6667]
+   * 
+   * @throws com.github.celldynamics.quimp.plugin.QuimpPluginException QuimpPluginException
+   */
+  @SuppressWarnings("serial")
+  @Test
+  public void test_getInterpolationMean() throws QuimpPluginException {
+    MedianSnakeFilter_ in = new MedianSnakeFilter_();
+    in.attachData(testcase);
+    Integer window = 3;
+    in.setPluginConfig(new ParamList() {
+      {
+        put("Window", String.valueOf(window));
+      }
+    });
+    double[] expected =
+            { 4.3333, 2.0000, 3.0000, 4.0000, 5.0000, 6.0000, 7.0000, 8.0000, 9.0000, 6.6667 };
+
+    List<Point2d> out;
+    out = in.runPlugin();
+    LOGGER.debug("org     : " + testcase.toString());
+    LOGGER.debug("Window 3: " + out.toString());
+
+    for (int i = 0; i < 10; i++) {
+      assertEquals(expected[i], out.get(i).getX(), 1e-4);
+      assertEquals(out.get(i).getX(), out.get(i).getY(), 1e-6);
+    }
+
+  }
+
+}
